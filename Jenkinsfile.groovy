e337c33071c9a006108a84c7b810a22d087b8619
y-mouak <y.mouak@attijariwafa.com>
2022-11-08 10:48:56 +0000

    first commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,25 @@
+node {
+
+    
+
+ try {
+
+     stage ('premier etape ') {
+
+         sh "echo 'premier etape pipiline ='"
+
+     }
+
+     
+
+ }    
+
+ finally {
+
+      cleanWs()
+
+  }
+
+  
+
+   }
