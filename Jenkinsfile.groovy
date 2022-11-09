b8201e83c6ae67f669200a40c91d16bf2f6842a1
y-mouak <y.mouak@attijariwafa.com>
2022-11-09 14:06:44 +0000

    ajout partie github

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,25 +1,83 @@
+def project_token = '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'
+
+properties([
+
+
+    gitLabConnection('si_connection'),
+
+    pipelineTriggers([
+
+        [
+
+            $class: 'GitHubPushTrigger',
+
+            branchFilterType: 'All',
+
+            triggerOnPush: true,
+
+            triggerOnMergeRequest: true,
+
+            triggerOpenMergeRequestOnPush: "never",
+
+            triggerOnNoteRequest: true,
+
+            noteRegex: "Jenkins please retry a build",
+
+            skipWorkInProgressMergeRequest: true,
+
+            secretToken: project_token,
+
+            ciSkip: false,
+
+            setBuildDescription: true,
+
+            addNoteOnMergeRequest: true,
+
+            addCiMessage: true,
+
+            addVoteOnMergeRequest: true,
+
+            acceptMergeRequestOnSuccess: true,
+
+            branchFilterType: "NameBasedFilter",
+
+            includeBranchesSpec: "",
+
+            excludeBranchesSpec: "",
+
+        ]
+
+    ])
+
+])
+
+
+node{
+
+    cleanWs()
+
+    try{
+
+        stage('PremiereEtape'){
+
+            sh "echo 'Hello World !!'"
+
+        }
+
+        stage('DeuxiemeEtape'){
+
+            sh "echo 'Hello World !!'"
+
+        }
+
+    }
+
+    finally{
+
+        cleanWs()
+
+    }
+
+}
+
 node {
-
-    
-
- try {
-
-     stage ('premier etape ') {
-
-         sh "echo 'premier etape pipiline ='"
-
-     }
-
-     
-
- }    
-
- finally {
-
-      cleanWs()
-
-  }
-
-  
-
-   }
